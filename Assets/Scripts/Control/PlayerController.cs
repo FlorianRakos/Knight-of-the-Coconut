5576c0a290cd2b8a70e7b963142dc969cb226a06
Florian167 <florian.rakos@yahoo.com>
2021-02-06 18:15:02 +0100

    Prototyping, Basic Movement, Animations

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    PlayerMovement playerMovement;
+
+
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            CastRay();
+        } 
+    }
+
+        private void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit)
+        {
+            Vector3 destination = hit.point;
+            
+            playerMovement.MoveTo(destination);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        UpdateAnimator();        
+    }
+
+
+
+
+
+    public void MoveTo(Vector3 destination)
+    {
+        GetComponent<NavMeshAgent>().SetDestination(destination);
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        float speed = localVelocity.z;
+
+        GetComponentInChildren<Animator>().SetFloat("forwardSpeed", speed);
+
+    }
+}
